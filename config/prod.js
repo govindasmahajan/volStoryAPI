c243107d8f127382129f6170bcd134d76900cebd
Govinda Mahajan <govindam@pune.smartek21.st21>
2019-02-08 15:50:20 +0530

    adding MLAB db config

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
     mongo: {
-        'url': 'mongodb://127.0.0.1:27017/VolStory',
+        'url': 'mongodb://govidasmahajan:Incorrect@123@ds227255.mlab.com:27255/volstory',
         'options': {
             server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
             replset: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
--- services/index.js
+++ services/index.js
@@ -2,14 +2,19 @@ const mongoose = require('mongoose');
 const dbConfig = require('../config/config').mongo;
 mongoose.Promise = global.Promise;
 
-mongoose.connect(dbConfig.url, {
-    useNewUrlParser: true
-}).then((res) => {
+if (process.env.NODE_ENV == "PRODUCTION") {
+    mongoose.connect(process.env.MLAB_URL)
     console.log("Successfully connected to the database");
-}).catch(err => {
-    console.log('Could not connect to the database. Exiting now...', err);
-    process.exit();
-});
+} else {
+    mongoose.connect(dbConfig.url, {
+        useNewUrlParser: true
+    }).then((res) => {
+        console.log("Successfully connected to the database");
+    }).catch(err => {
+        console.log('Could not connect to the database. Exiting now...', err);
+        process.exit();
+    });
+}
 
 const eventService = require('./events/eventServices');
 
